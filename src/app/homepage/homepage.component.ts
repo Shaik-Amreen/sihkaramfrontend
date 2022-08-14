6884dcfa018ff44c6b42cdc9ef7f99add763ebfc
Shaik-Amreen <69184237+Shaik-Amreen@users.noreply.github.com>
2022-08-14 11:38:28 +0530

    homepage changeone

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -8,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() {}
+  images: any = ['assets/homeone.jpg', 'assets/hometwo.jpg', 'assets/homefou.jpg']
+  imageSource: any = this.images[0]
+  i: any = 0
+  constructor() {
+    setInterval(() => {
+      this.imageSource = this.images[this.i];
+      if (this.i == this.images.length - 1) {
+        this.i = 0
+      }
+      else {
+        this.i++;
+      }
+
+    }, 3000)
+
+  }
 
   ngOnInit(): void {
   }
