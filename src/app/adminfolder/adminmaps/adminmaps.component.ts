7a102dc0c396c739ce20d738d42637ff2065ff11
Shaik-Amreen <69184237+Shaik-Amreen@users.noreply.github.com>
2022-08-24 23:48:22 +0530

    enrollform

--- src/app/adminfolder/adminmaps/adminmaps.component.ts
+++ src/app/adminfolder/adminmaps/adminmaps.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttprequestService } from '../../commonservices/httprequest.service';
 
 @Component({
@@ -14,6 +14,9 @@ export class AdminmapsComponent implements OnInit {
   err = false;
   submitStatus = false
   data: any = []
+  addPeople: any = false
+  people: FormGroup
+  userStatus: any = ''
 
   constructor(private httprequest: HttprequestService,) {
     this.mapData = new FormGroup({
@@ -24,8 +27,15 @@ export class AdminmapsComponent implements OnInit {
       iframe: new FormControl(''),
       located: new FormControl('', Validators.required),
       slumid: new FormControl('', Validators.required),
+      people: new FormArray([])
     })
 
+
+    this.people = new FormGroup({
+      slumid: new FormControl('', Validators.required),
+      id: new FormControl('', Validators.required),
+      fullname: new FormControl('', Validators.required)
+    })
 
     this.getData()
 
@@ -40,24 +50,40 @@ export class AdminmapsComponent implements OnInit {
     )
   }
 
+  add(i: any) {
+    // console.log(i)
+
+    this.userStatus = ''
+    this.addPeople = true
+    this.people.value.slumid = i.slumid
+    this.display = 'block';
+    this.mapData.patchValue(i)
+    // this.mapData.controls.slumid.disable()
+  }
   edit(i: any) {
+    this.err = false
+    this.addPeople = false
     this.editMode = true
     this.display = 'block';
     this.mapData.patchValue(i)
-    console.log(this.mapData.value,"edit activate")
+    console.log(this.mapData.value, "edit activate")
     // this.prevslumid = this.mapData.controls.slumid
     this.mapData.controls.slumid.disable()
-    
+
   }
 
-  close(){
-      this.mapData.reset();
+  close() {
+    this.mapData.reset();
+  }
+  closePeople() {
+    this.display = 'None'
   }
 
-  display: any = 'None';prevslumid:any
+  display: any = 'None'; prevslumid: any
   displaymodal() {
     this.mapData.reset();
     this.editMode = false
+    this.addPeople = false
     this.display = 'block';
     this.mapData.controls.slumid.enable();
   }
@@ -66,6 +92,7 @@ export class AdminmapsComponent implements OnInit {
   }
 
   submit() {
+    this.err = false
     if (this.mapData.status == 'VALID') {
       this.mapData.controls.slumid.enable();
       let url = '/postMaps'
@@ -75,7 +102,6 @@ export class AdminmapsComponent implements OnInit {
         // data.prevslumid=this.prevslumid
       }
       // console.log(url,data)
-      console.log(this.mapData.value,"postrequest....................")
       this.httprequest
         .postrequest(url, this.mapData.value)
         .subscribe((res: any) => {
@@ -87,6 +113,23 @@ export class AdminmapsComponent implements OnInit {
     }
     else {
       this.err = true;
+    }
+  }
+
+  enroll() {
+    this.err = false
+    if (this.people.status === 'VALID') {
+      this.httprequest
+        .postrequest('/findOrPostPeople', this.mapData.value)
+        .subscribe((res: any) => {
+          if (res.message == 'exist') {
+            this.userStatus = 'User already exist'
+          }
+          else { this.display = 'None'; }
+        });
+    }
+    else {
+      this.err = true
     }
   }
 
